f2512522833f4613874d1624053e3d436e39b2da
noenadies <33671023+noenadies@users.noreply.github.com>
2018-05-29 16:29:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+myFunction();
+function myFunction() {
+
+
+
+			  $.ajax({
+               
+                url:   'web.json',
+                 dataType:  'json',
+                type:  'post',
+                cache:false,
+                success:  function (data) { 
+                  var as=Object.keys(data.inicio);
+                  for(var i=0; i<as.length;i=i+1){
+                  //  data.inicio[i].web
+                document.getElementById("iframe").src=data.inicio[i].web
+                  
+                  }
+                }
+                });
+      }
