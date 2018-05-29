b7a1b8c16d82bc64f3a7dc798e1d0eed54906700
noenadies <33671023+noenadies@users.noreply.github.com>
2018-05-29 16:43:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function myFunction() {
                   var as=Object.keys(data.inicio);
                   for(var i=0; i<as.length;i=i+1){
                   //  data.inicio[i].web
-                document.getElementById("iframe").src=data.inicio[i].web
-                  
+                document.getElementById("iframe").src=data.inicio[i].web;
+                  alert(ata.inicio[i].web);
                   }
                 }
                 });
